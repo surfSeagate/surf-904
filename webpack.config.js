3801887cd24c06b5dff9f271dae4704bacda962c
Glenn Curry <surf13jax@gmail.com>
2018-12-19 19:03:12 -0500

    more changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const path = require("path");
 //console.log(path.join(__dirname, 'public'));
 
 module.exports = {
-  // entry: "./src/app.js",
-  entry: "app.js",
+  entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
